437a7adbf73393ad4a714b61d10cb53add9679b2
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-08-17 15:41:31 +0800

    Adding note

--- src/main/java/com/example/B40API/B40ApiApplication.java
+++ src/main/java/com/example/B40API/B40ApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class B40ApiApplication {
 
+	//Note
 	public static void main(String[] args) {
 		SpringApplication.run(B40ApiApplication.class, args);
 	}
